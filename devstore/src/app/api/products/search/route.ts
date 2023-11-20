f1806fb75ea96687baf5340a4e54be075866c76a
Zenky <bzenky@gmail.com>
2023-11-20 11:46:52 -0300

    search loading

--- devstore/src/app/api/products/search/route.ts
+++ devstore/src/app/api/products/search/route.ts
@@ -3,7 +3,7 @@ import data from '../data.json'
 import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
+  await new Promise((resolve) => setTimeout(resolve, 2000))
 
   const { searchParams } = request.nextUrl
   const query = z.string().parse(searchParams.get('q'))
